7f2cd155a59ed6ca2837368861f08bbf7098a127
Bruna Delmouro <brunadelmouro@gmail.com>
2021-08-27 19:32:33 -0300

    create project

---
+++ src/test/java/br/com/brunadelmouro/cursospringboot/CursospringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.brunadelmouro.cursospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursospringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
