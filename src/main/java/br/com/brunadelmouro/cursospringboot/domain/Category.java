e141df6a9a5d26cd54f4cf9020c65fc4effad6b6
Bruna Delmouro <brunadelmouro@gmail.com>
2021-08-27 21:12:40 -0300

    domain - Category

---
+++ src/main/java/br/com/brunadelmouro/cursospringboot/domain/Category.java
@@ -0,0 +1,49 @@
+package br.com.brunadelmouro.cursospringboot.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+//add lombok
+public class Category implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private Integer id;
+    private String name;
+
+    public Category() {
+    }
+
+    public Category(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return Objects.equals(id, category.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
--- src/main/java/br/com/brunadelmouro/cursospringboot/resources/CategoryResource.java
+++ src/main/java/br/com/brunadelmouro/cursospringboot/resources/CategoryResource.java
@@ -1,8 +1,12 @@
 package br.com.brunadelmouro.cursospringboot.resources;
 
+import br.com.brunadelmouro.cursospringboot.domain.Category;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categories")
@@ -10,7 +14,12 @@ public class CategoryResource {
 
 
     @RequestMapping(method= RequestMethod.GET) // HTTP request
-    public String listar(){
-        return "REST is working";
+    public List<Category> list(){
+
+        List<Category> categories = new ArrayList<>();
+        categories.add(new Category(1,"Informática"));
+        categories.add(new Category(2,"Escritório"));
+
+        return categories;
     }
 }
