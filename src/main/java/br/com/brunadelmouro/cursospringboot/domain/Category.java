c6c1985de3d7d732fae64ae85c63f5ec8a8b17e7
Bruna Delmouro <brunadelmouro@gmail.com>
2021-08-29 16:04:24 -0300

    add H2 database | JPA

--- src/main/java/br/com/brunadelmouro/cursospringboot/domain/Category.java
+++ src/main/java/br/com/brunadelmouro/cursospringboot/domain/Category.java
@@ -1,12 +1,20 @@
 package br.com.brunadelmouro.cursospringboot.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
 //add lombok
-public class Category implements Serializable {
 
+@Entity
+public class Category implements Serializable {
     private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Integer id;
     private String name;
 
